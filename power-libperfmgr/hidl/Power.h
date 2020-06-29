11d5f03cdbbd4daab0c13d6489c7a5938c6f92b2
Jesse Chan <jc@lineageos.org>
2020-06-29 12:26:29 +0800

    msm8998-common: power-libperfmgr: remove Google-specific camera and audio hints
    Change-Id: I65285ca8c7248bd1e55d3800862e96c00f00effd
    Signed-off-by: 0ranko0P <ranko0p@outlook.com>

--- power-libperfmgr/hidl/Power.h
+++ power-libperfmgr/hidl/Power.h
@@ -26,7 +26,6 @@
 #include <hidl/Status.h>
 #include <perfmgr/HintManager.h>
 
-#include "CameraMode.h"
 #include "disp-power/InteractionHandler.h"
 
 namespace android {
@@ -70,7 +69,7 @@ class Power : public IPower {
     std::shared_ptr<HintManager> mHintManager;
     std::unique_ptr<InteractionHandler> mInteractionHandler;
     std::atomic<bool> mSustainedPerfModeOn;
-    std::atomic<enum CameraStreamingMode> mCameraStreamingMode;
+    std::atomic<bool> mCameraStreamingMode;
     std::atomic<bool> mReady;
     std::thread mInitThread;
 };
