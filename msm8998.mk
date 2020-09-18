f8008dc86102896a13a01b02daaea99c7627e57a
LuK1337 <priv.luk@gmail.com>
2020-09-18 14:36:05 +0200

    msm8998-common: Build libhidltransport/libhwbinder
    * Needed for old blobs
    
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172
    Signed-off-by: 0ranko0P <ranko0p@outlook.com>

--- msm8998.mk
+++ msm8998.mk
@@ -244,6 +244,13 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
+
+# HIDL
+PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
