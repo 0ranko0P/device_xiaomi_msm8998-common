2fcf2a8dd857a1247b97fb607b8f0fcf54538e7a
Han Wang <416810799@qq.com>
2020-10-29 18:38:04 +0600

    Revert "Force enable mobile network settings v2"
    * This prop is not honored anymore on 11.
    
    This reverts commit def3cb34a1b7fe946d72bdca3db168efa257e0a2.
    
    Signed-off-by: 0ranko0P <ranko0p@outlook.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -152,8 +152,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
-    persist.vendor.qti.telephony.vt_cam_interface=1 \
-    persist.sys.fflag.override.settings_network_and_internet_v2=true
+    persist.vendor.qti.telephony.vt_cam_interface=1
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
