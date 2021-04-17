528af84732461837b55d0c3bc03f6c175b999bde
0ranko0P <ranko0p@outlook.com>
2021-04-17 18:18:49 +0800

    msm8998-common: Move blur props into system default properties
    * Blocked by SELinux

--- system_prop.mk
+++ system_prop.mk
@@ -25,4 +25,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Graphics
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.hardware.egl=adreno \
-    ro.hardware.vulkan=adreno
+    ro.hardware.vulkan=adreno \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
