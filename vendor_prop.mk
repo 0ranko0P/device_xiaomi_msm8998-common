ee5df63ac2c42a38300919bae03a24b19fea0b48
Kyle Harrison <khwebmail@gmail.com>
2021-10-14 10:00:52 +0100

    msm8998-common: properties: Use threaded GLES render engine
    Change-Id: Id1feabf037b2d1648ac55aafc83c1b624beaa373

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -132,6 +132,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_hwc_vds=0 \
+    debug.renderengine.backend=threaded \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=0 \
     ro.opengles.version=196610 \
