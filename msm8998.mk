c00952a04e891361985dd258327f7e74001223dd
0ranko0P <ranko0p@outlook.com>
2021-10-22 22:43:16 +0800

    msm8998-common: Drop duplicate nfc config

--- msm8998.mk
+++ msm8998.mk
@@ -339,7 +339,6 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.1-service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # OMX
