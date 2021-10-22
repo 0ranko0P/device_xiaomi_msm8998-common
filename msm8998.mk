b3c11c7da984d3da2e8274a1ac2bdf9f198984c4
0ranko0P <ranko0p@outlook.com>
2021-10-22 21:47:03 +0800

    Revert "Revert "msm8998-common: vndk: Enable runtime isolation""
    This reverts commit de7c4bab8cb39d83616f941e1ea0c0da30afd267.

--- msm8998.mk
+++ msm8998.mk
@@ -445,6 +445,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
+# VNDK
+PRODUCT_PACKAGES += \
+    libdng_sdk.vendor_32 \
+    libstdc++.vendor_32 \
+    vndk-ext
+
 # Wifi
 PRODUCT_PACKAGES += \
     ipacm \
