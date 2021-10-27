55e387e0ff0b1e319f0b9f2dd776e78f799819a7
sbwml <984419930@qq.com>
2021-10-27 20:00:04 +0800

    msm8998-common: Update cne,qmi,ims,ril blobs

--- msm8998.mk
+++ msm8998.mk
@@ -393,6 +393,7 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc
 
 PRODUCT_PACKAGES += \
+    init.class_main.sh \
     init.panel_info.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
@@ -415,8 +416,11 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.deprecated@1.0 \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.0 \
-    android.hardware.secure_element@1.0.vendor
-    librmnetctl
+    android.hardware.secure_element@1.0.vendor \
+    CarrierConfigOverlay \
+    rild \
+    librmnetctl \
+    libprotobuf-cpp-full
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
