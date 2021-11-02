4abe0d51bd2393c59517dfc99e1dd98579a2f465
Albert I <kras@raphielgang.org>
2021-11-02 21:47:53 +0530

    msm8998-common: Makefile: Inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.
    
    Change-Id: I088fd00500ce10df5f413cb762aaed2ec00f5dfc

--- msm8998.mk
+++ msm8998.mk
@@ -489,6 +489,10 @@ PRODUCT_PACKAGES += \
     libnl \
     libmediaextractor
 
+# Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 include vendor/xiaomi/msm8998-common-extra/wfd.mk
 
 # Call the proprietary setup
