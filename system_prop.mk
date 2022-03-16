bfec2080db8ffc12e7fbb1c4b5ca1de01a02f7e4
0ranko0P <ranko0p@outlook.com>
2022-03-16 18:02:34 +0800

    msm8998-common: Disable IMS logs in user product
    * Copy from AOSPA: https://github.com/AOSPA/android_device_qcom_common/blob/sapphire/system/telephony/qti-telephony.mk

--- system_prop.mk
+++ system_prop.mk
@@ -29,3 +29,14 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.hardware.vulkan=adreno \
     ro.sf.blurs_are_expensive=1 \
     ro.surface_flinger.supports_background_blur=0
+
+ifeq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.vendor.ims.disableADBLogs=1 \
+    persist.vendor.ims.disableDebugLogs=1 \
+    persist.vendor.ims.disableIMSLogs=1 \
+    persist.vendor.ims.disableQXDMLogs=1 \
+    persist.vendor.ims.loglevel=0 \
+    persist.vendor.ims.rtp.enableqxdm=0 \
+    persist.vendor.ims.vt.enableadb=0
+endif
